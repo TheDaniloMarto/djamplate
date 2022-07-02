e3e71f9c49c7e15255da7218bbc9ed9cf5defaac
Danilo Marto de Carvalho <dmcarvalho.dmc@yandex.com>
2022-07-02 00:42:50 -0300

    ft: Delete all files

--- README.md
+++
@@ -1,8 +0,0 @@
-# PJ001
-
-A simple web app built with Django
-
-## LICENSE
-
-The MIT LICENSE
-
--- apps/core/views.py
+++
@@ -1,8 +0,0 @@
-from django.views.generic import TemplateView
-
-
-class Index(TemplateView):
-    template_name = "pages/index.html"
-
-
-index = Index.as_view()
